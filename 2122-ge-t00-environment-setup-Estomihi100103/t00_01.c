095235c86ef9e800b1872cf7273e4cee37a17ec3
Estomihi <89466828+Estomihi100103@users.noreply.github.com>
2023-05-03 10:35:32 +0700

    Push

---
+++ 2122-ge-t00-environment-setup-Estomihi100103/t00_01.c
@@ -0,0 +1,13 @@
+//12S21004-Estomihi Pangaribuan
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int _argv, char **_argc)
+{
+    printf("Aloha!");
+
+    return 0;
+}
+
+
---
+++ 2122-ge-t00-environment-setup-Estomihi100103/t00_02.c
@@ -0,0 +1,11 @@
+// 12S21004-Estomihi Pangaribuan
+
+#include <stdio.h>
+
+int main(int _argv, char **_argc)
+{
+    printf("Aloha!");
+
+    return 0;
+}
+
